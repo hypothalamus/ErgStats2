74e538a8d34161a2d11a17fb46c8a6f2f0a6f80e
Larissa Schrempp <larissas@student.ethz.ch>
2018-11-11 00:40:38 +0100

    Added toolbars

--- app/src/main/java/com/ergapp/laris/ergstats/ManualInput.java
+++ app/src/main/java/com/ergapp/laris/ergstats/ManualInput.java
@@ -1,7 +1,11 @@
 package com.ergapp.laris.ergstats;
 
+import android.content.Intent;
+import android.graphics.drawable.Icon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 public class ManualInput extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class ManualInput extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_input);
+
+        Toolbar bar = findViewById(R.id.tool_man);
+        bar.setTitle("Add a manual activity");
+        setSupportActionBar(bar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 }
