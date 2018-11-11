cfcc08115cd947571e4faead7dc5326eaecb3edd
Larissa Schrempp <larissas@student.ethz.ch>
2018-11-11 18:24:31 +0100

    Added DatePicker

--- app/src/main/java/com/ergapp/laris/ergstats/ManualInput.java
+++ app/src/main/java/com/ergapp/laris/ergstats/ManualInput.java
@@ -1,13 +1,24 @@
 package com.ergapp.laris.ergstats;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.drawable.Icon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.support.v7.widget.Toolbar;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class ManualInput extends AppCompatActivity {
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class ManualInput extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,32 @@ public class ManualInput extends AppCompatActivity {
         setSupportActionBar(bar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
 
+    public static class DatePickerFragment extends DialogFragment {
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+        }
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar cal = new GregorianCalendar(year, month, day);
+        setDate(cal);
+    }
+
+    public void setDate (final Calendar calendar) {
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        ((TextView) findViewById(R.id.showDate)).setText(dateFormat.format(calendar.getTime()));
+    }
+
+    public void datePicker (View view) {
+        DatePickerFragment fragment = new DatePickerFragment();
+        FragmentManager f = getFragmentManager();
+        fragment.show(f, "date");
     }
 }
